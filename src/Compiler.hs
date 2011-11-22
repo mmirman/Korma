b93050272056bb9cd8599c63bf31daaa1cecaf17
Matthew Mirman <mmirman@andrew.cmu.edu>
2011-11-22 13:29:47 -0700

    Added some documentation.

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -1,36 +1,55 @@
 {-# LANGUAGE
  GADTs,
  StandaloneDeriving,
- ExistentialQuantification
+ ExistentialQuantification,
+ ScopedTypeVariables
  #-}
 
 module Compiler where
 
 data Litteral
-
-
-data Exp a where
-  EApp    :: Exp a -> Exp b -> Exp Litteral
-  ELam    :: Exp b -> Exp a
-  EVar    :: Int -> Exp a  
-  ECons   :: Int -> Exp a
-  EChoose :: [(Int, Exp b)] -> Exp a
-  
+data Value
 data Heap
 data Stack
 
 type HeapT = [Elem Heap]
 type StackT = [Elem Stack]
-data Elem a where
-  Closure   :: HeapT -> Exp b -> Elem a
-  FChoose   :: HeapT -> [(Int,Exp b)] -> Elem Stack
-  Reference :: HeapT -> Int -> Elem Stack
-  FProd     :: [Elem Stack] -> Elem Heap  
-  Value     :: Exp b -> Elem Heap
+
 data CState where 
   CState :: HeapT -> Exp a -> StackT -> CState
   Done :: Exp a -> CState
-  
+
+data Elem a where
+  Closure   :: HeapT -> Exp b -> Elem a
+  FChoose   :: HeapT -> [(Int, Exp a)] -> Elem Stack
+  Reference :: HeapT -> Int -> Elem Stack
+  Value     :: Exp a -> Elem Heap
+
+data Exp a where
+  EApp    :: Exp a -> Exp b -> Exp Litteral
+  ELam    :: Exp a -> Exp a
+  EVar    :: Int -> Exp a
+  EVal    :: Show b => b -> Exp a
+  ECons   :: Int {- ^ name -} -> StackT -> Exp a
+  EChoose :: [(Int, Exp a)] -> Exp a
+
+($$) = EApp
+
+infixl 2 <$ 
+(<$) = EApp
+infixr 1 $>
+($>) = EApp
+
+fix fun = (ELam$ fxx $$ fxx) $$ fun
+  where fxx = (ELam$ f $$ (x $$ x) )
+        f = EVar 1
+        x = EVar 0
+        
+eval :: Exp Litteral -> String
+eval l = eval' (CState [] l [])
+  where eval' (Done a) = show a
+        eval' f = eval' $ evaluate f
+
 evaluate :: CState -> CState
 evaluate (Done a) = error $ "Finished: "++show a 
 evaluate (CState hp e st) = case (e, st) of
@@ -49,24 +68,28 @@ evaluate (CState hp e st) = case (e, st) of
   
   (EVar _, Reference hp' x:sr) -> -- VAL
     CState (replace (Value e) x hp') e sr
-  
+    
   (_, Reference hp' x:sr) -> -- MEMOIZE
     CState (replace (Closure hp e) x hp') e sr
-  
+    
   (ELam a, Closure hp' b:sr) -> -- LAM
     CState (Closure hp' b:hp) a sr
-  
-  (EChoose l, Closure hp' a:sr) ->
+    
+  (EChoose l, Closure hp' a:sr) -> 
+    -- this is probably not the smartest way of doing this, but whatevs.
+    -- in a real - non anonymous program, we'd know how many inputs an ADT has.
+    -- and can thus write "static" code to handle it.
     CState hp' a $ FChoose hp l:sr
     
-  (ECons i, FChoose hp' l:sr) | Just b <- lookup i l
-                              , FProd args:_ <- hp ->
-    CState hp' b (args++sr)
-
-  (ECons i, p:sr) -> case hp of
-    FProd args:l -> CState ((FProd $ p:args):hp) (ECons i) sr
-
+  (ECons i l, FChoose hp' l':sr) -> case lookup i l' of
+    Just a -> CState hp' a $ reverseConcat  l sr    
+    Nothing -> error "type error"
     
+  (ECons i l, a:sr) ->
+    CState hp (ECons i $ a:l) sr
+  
+reverseConcat [] l = l
+reverseConcat (a:r) l = reverseConcat r $ a:l
 
 replace a 0 (v:l) = a:l
 replace _ i [] = error $ "index "++show i++" is out of bounds"
@@ -116,3 +139,14 @@ deriving instance Show (Exp a)
 deriving instance Show (Elem a)
 deriving instance Show Heap
 deriving instance Show Stack
+
+{-
+data Exp a where
+  EApp    :: Exp a -> Exp b -> Exp Litteral
+  ELam    :: Exp a -> Exp a
+  EVar    :: Int -> Exp a
+  EVal    :: Show b => b -> Exp a
+  ECons   :: Int {- ^ name -} -> StackT -> Exp a
+  EChoose :: [(Int, Exp a)] -> Exp a
+
+-}
